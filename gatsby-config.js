7d6e8b38fed6c16938480ac2b1e78b0adb9a5724
doberman7 <ivanrubioangeles@gmail.com>
2021-03-17 16:54:53 -0600

    TypeError: Cannot read property 'allContentfulBlogPost' of undefined

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
