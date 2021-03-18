724b9e3f74eaa9a5d8830a9ddc20bd20f87d621a
doberman7 <ivanrubioangeles@gmail.com>
2021-03-18 11:22:09 -0600

    can se home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,59 @@
-// import React from "react";
-// import { graphql } from "gatsby";
-// import get from "lodash/get";
-// import { Helmet } from "react-helmet";
-// import Hero from "../components/hero";
-// import Layout from "../components/layout";
-// import ArticlePreview from "../components/article-preview";
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import { Helmet } from "react-helmet";
+import Hero from "../components/hero";
+import Layout from "../components/layout";
+import ArticlePreview from "../components/article-preview";
 
-// class RootIndex extends React.Component {
-//   render() {
-//     const siteTitle = get(this, "props.data.site.siteMetadata.title");
-//     const posts = get(this, "props.data.allContentfulBlogPost.edges");
-//     const [author] = get(this, "props.data.allContentfulPerson.edges");
+class RootIndex extends React.Component {
+  render() {
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    const posts = data.allContentfulPost.edges;
+    // const [author] = get(this, "props.data.allContentfulPerson.edges");
 
-//     return (
-//       <Layout location={this.props.location}>
-//         <div style={{ background: "#fff" }}>
-//           <Helmet title={siteTitle} />
-//           <Hero data={author.node} />
-//           <div className="wrapper">
-//             <h2 className="section-headline">Recent articles</h2>
-//             <ul className="article-list">
-//               {posts.map(({ node }) => {
-//                 return (
-//                   <li key={node.slug}>
-//                     <ArticlePreview article={node} />
-//                   </li>
-//                 );
-//               })}
-//             </ul>
-//           </div>
-//         </div>
-//       </Layout>
-//     );
-//   }
-// }
+    return (
+      <Layout location={this.props.location}>
+        <div style={{ background: "#fff" }}>
+          <Helmet title={siteTitle} />
+          <div className="wrapper">
+            <h2 className="section-headline">Recent articles</h2>
+            <ul className="article-list">
+              {posts.map(({ node }) => {
+                return (
+                  <li key={node.slug}>
+                    <ArticlePreview article={node} />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
 
-// export default RootIndex;
+export default RootIndex;
 
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `;
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+
+    allContentfulPost {
+      edges {
+        node {
+          title
+          subtitle
+          author
+          slug
+        }
+      }
+    }
+  }
+`;
